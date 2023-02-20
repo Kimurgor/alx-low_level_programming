df11935f4dd9f59f1f6514eb13cc3bf3f5e60719
Cyrus Kimurgor <kimurgorcyrus10@gmail.com>
2023-02-19 21:38:58 -0800

    Quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <unistd.h>
-/**
- * Main
- * Return 1
- */
-int main()
-{
-    char *message = "and that piece of art is useful\" -" + 
-	    "Dora Korpar, 2015-10-19\n";
-    write(2, message, 59);
-    return 1;
-}
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
