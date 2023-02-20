206b9514d3e432ed3ed2f8630ae41c4ca4e0c5f5
Cyrus Kimurgor <kimurgorcyrus10@gmail.com>
2023-02-20 01:56:29 -0800

    Updated quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,7 @@
 
 int main(void)
 {
-	write(2, "and that piece good of art is useful\" - Dora Korpar, 2015-10-19\n", 63);
+	write(2, "and that piece good of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
