80f04085f301a296b0513c9fe5b31de107ee2047
Cyrus Kimurgor <kimurgorcyrus10@gmail.com>
2023-03-20 05:56:34 -0700

    Update

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Name of file of compilation
+ * Description  - _FILE_
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("Name of file : %s\n", __FILE__);
+	return (0);
+}
