f649b03e042f79a97d3752c86d9a818e0653e39f
Cyrus Kimurgor <kimurgorcyrus10@gmail.com>
2023-03-22 11:33:50 -0700

    Update

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,28 +1,61 @@
 #include "3-calc.h"
-#include <stdlib.h>
 
 /**
- * get_op_func - Selects the correct function to perform
- *               the operation asked by the user.
- * @s: The operator passed as argument.
+ * op_add - addition for two integers
+ * @a: first integer
+ * @b: second integer
  *
- * Return: A pointer to the function corresponding
- *         to the operator given as a parameter.
+ * Return: the sum of a  and b.
  */
-int (*get_op_func(char *s))(int, int)
+int op_add(int a, int b)
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i = 0;
+	return (a + b);
+}
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
-		i++;
+/**
+ * op_sub - difference for two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: difference of a and b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
 
-	return (ops[i].f);
+/**
+ * op_div - a division function
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result of the division of a and b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mul - finds the product
+ * @a: integer
+ * @b: integer
+ *
+ * Return: the product of a and b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_mod - finds remainder
+ * @a: integer
+ * @b: integer
+ *
+ * Return: the remainder of the division of a by b.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
 }
